91e155abdf9f4c3ca5d6eb72b386c7168b36141e
alex <desorbaix@free.fr>
2021-04-27 15:46:54 +0200

    propagation de style

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import Button from './components/Button/Button';
-const uneVar='Demat Breizh';
+const uneVar = 'Demat Breizh';
 function App() {
   return (
     <div className="App">
-     <Button label="Ok" lorsqueJeClickSurLeBoutton={(value)=>{
-       console.log('J\'ai ete clicke donc fais qqch stp');
-       alert('Button clicked!!!!!!!!!');
-       console.log('L\'alert est fermée')
-     }}/>
-     <Button label="Cancel" couleurDeFond={'tomato'} taillePolice={24} lorsqueJeClickSurLeBoutton={(value)=>{}}/>
-     <Button label="Don't know" couleurDeFond="skyblue" lorsqueJeClickSurLeBoutton={(value)=>{}}/>
+      <Button label="Ok"
+        lorsqueJeClickSurLeBoutton={(value) => {
+          console.log('J\'ai ete clicke donc fais qqch stp');
+        }}
+        style={{ textDecoration: 'underline', fontStyle: 'italic' }} />
+      <Button label="Cancel" couleurDeFond={'tomato'} taillePolice={24} lorsqueJeClickSurLeBoutton={(value) => { }} />
+      <Button label="Don't know" couleurDeFond="skyblue" lorsqueJeClickSurLeBoutton={(value) => { }} />
     </div>
   );
 }
--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Button.css'
-const Button=(props) => {
+const Button = (props) => {
     console.log(props)
-    return <div style={{backgroundColor:props.couleurDeFond,fontSize:props.taillePolice+'px'}} className="Button" onClick={(evt)=>{props.lorsqueJeClickSurLeBoutton(true)}}>{props.label}</div>
+    return <div style={{
+        ...props.style,
+        backgroundColor: props.couleurDeFond,
+        fontSize: props.taillePolice + 'px'
+    }}
+        className="Button"
+        onClick={(evt) => { props.lorsqueJeClickSurLeBoutton(true) }}
+    >
+        {props.label}
+    </div>
 };
+Button.propTypes = {
+    label: PropTypes.string.isRequired,
+    couleurDeFond: PropTypes.string,
+    taillePolice: PropTypes.number,
+    lorsqueJeClickSurLeBoutton: PropTypes.func.isRequired,
+}
+Button.defaultProps = {
+    taillePolice: 32,
+    couleurDeFond: 'green'
+}
 export default Button;
