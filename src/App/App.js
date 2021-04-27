8ce4d1076fb6707b6fd6ae9381ca9e2db8f964ab
alex <desorbaix@free.fr>
2021-04-27 14:51:27 +0200

    restruct de l'app + vidange

---
+++ src/App/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+const uneVar='Demat Breizh';
+function App() {
+  return (
+    <div className="App">
+     {uneVar}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App/components/Button/Button.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Button=() => {
+    return <div>Hello world</div>
+}
